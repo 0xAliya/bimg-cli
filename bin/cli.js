3a26e50d36892ef9f30c7c707b9b34497ef7184e
PenLa <penpenla@outlook.com>
2021-06-26 16:36:10 +0800

    clean

--- bin/cli.js
+++ bin/cli.js
@@ -10,8 +10,8 @@ let index = 0;
 
 const file2Base64 = (filePath) => fs.readFileSync(filePath).toString('base64');
 
-const uploadFileToGithub = async (content, fileType, { token, owner, repo, path }) =>
-  await octokit.request(`PUT https://api.github.com/repos/{owner}/{repo}/contents/{path}/${Date.now()}${index++}.${fileType}`, {
+const uploadFileToGithub = (content, fileType, { token, owner, repo, path }) =>
+  octokit.request(`PUT https://api.github.com/repos/{owner}/{repo}/contents/{path}/${Date.now()}${index++}.${fileType}`, {
     headers: {
       Authorization: `token ${token}`
     },
@@ -24,13 +24,13 @@ const uploadFileToGithub = async (content, fileType, { token, owner, repo, path 
     }
   });
 
-const uploadFileToGitte = async (content, fileType, { token, owner, repo, path }) =>
-  await octokit.request(`POST https://gitee.com/api/v5/repos/{owner}/{repo}/contents/{path}/${Date.now()}${index++}.${fileType}`, {
+const uploadFileToGitte = (content, fileType, { token, owner, repo, path }) =>
+  octokit.request(`POST https://gitee.com/api/v5/repos/{owner}/{repo}/contents/{path}/${Date.now()}${index++}.${fileType}`, {
     owner,
     repo,
     path,
     data: {
-      'access_token': `${token}`,
+      'access_token': token,
       'message': 'upload image',
       'content': content,
     }
@@ -48,24 +48,24 @@ program
 program
   .command('upload')
   .description('Upload images to github or gitee')
-  .option('-t, --token <token>', 'token')
   .option('--type <type>', 'git repo type', 'github')
+  .option('-t, --token <token>', 'token')
   .option('-o, --owner <owner>', 'owner')
   .option('-r, --repo <repo>', 'repo')
   .option('-p, --path <path>', 'path')
   .action(async (command) => {
     try {
-      const upload = uploadFile(command.type)
+      const upload = uploadFile(command.type);
       const args = command.args;
 
-      const result = await Promise.all(
+      const results = await Promise.all(
         args.map(filePath => upload(
           filePath, command
         ))
       );
 
-      result.forEach(item => {
-        console.log(item.data.content.download_url);
+      results.forEach(result => {
+        console.log(result.data.content.download_url);
       })
 
     } catch (error) {
